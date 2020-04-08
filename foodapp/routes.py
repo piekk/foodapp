e6989d7623b4582e22f75861cd06fe3a99274323
pie <pie.tiranon@gmail.com>
2020-04-08 11:13:36 +0700

    update 4/8

--- foodapp/routes.py
+++ foodapp/routes.py
@@ -5,3 +5,10 @@ from flask import render_template
 @app.route('/')
 def home():
   return render_template("home.html")
+
+@app.route('/<brand>')
+def view(brand):
+    # if brand in db:
+    #return Brand
+    #else
+  return render_template("brand.html",brand=brand)
