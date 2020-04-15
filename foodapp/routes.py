4d6e7fa5e9ae1807b64183140565389e36f036ca
pie <pie.tiranon@gmail.com>
2020-04-15 10:39:42 +0700

    update 4/15

--- foodapp/routes.py
+++ foodapp/routes.py
@@ -1,10 +1,19 @@
 import os
 from foodapp import app
-from flask import render_template
+from flask import render_template, request
 
 @app.route('/')
 def home():
-  return render_template("home.html")
+    user = request.cookies.get('user')
+    if user:
+        return render_template("user.html", user=user)
+    else:
+        return render_template("home.html")
+    #if user:
+        #check db for cart
+    #else:
+        #do nothing
+
 
 @app.route('/<brand>')
 def view(brand):
@@ -12,3 +21,11 @@ def view(brand):
     #return Brand
     #else
   return render_template("brand.html",brand=brand)
+
+@app.route('/shop')
+def shop():
+    return render_template("shop.html")
+
+@app.route('/product')
+def product():
+    return render_template("product.html")
